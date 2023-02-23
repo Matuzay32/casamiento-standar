6fdceac773c92944287067694a6aaab24b333f68
Matuzay32 <eze.rey92@gmail.com>
2023-02-23 00:36:14 -0300

    solucionando

--- components/HeroBackOffice.jsx
+++ components/HeroBackOffice.jsx
@@ -23,26 +23,28 @@ export default function HeroBackOffice({
 }) {
 	return (
 		<>
-			<Head>
+			{/* <Head>
 				<link
 					href="https://fonts.googleapis.com/css2?family=Caveat:wght@700&display=swap"
 					rel="stylesheet"
 				/>
-			</Head>
+			</Head> */}
 
 			<Container maxW={"3xl"}>
 				<Stack
 					as={Box}
 					textAlign={"center"}
 					spacing={{ base: 8, md: 14 }}
-					py={{ base: 20, md: 36 }}
-				>
+					py={{ base: 20, md: 36 }}>
 					<Heading
 						fontWeight={600}
-						fontSize={{ base: "4xl", sm: "4xl", md: "6xl" }}
+						fontSize={{
+							base: "4xl",
+							sm: "4xl",
+							md: "6xl",
+						}}
 						lineHeight={"110%"}
-						color={colorPrimerTitulo}
-					>
+						color={colorPrimerTitulo}>
 						Back office
 						<br />
 						<Text as={"span"} color={color}>
@@ -50,9 +52,12 @@ export default function HeroBackOffice({
 						</Text>
 					</Heading>
 					<Text
-						fontSize={{ base: "sm", md: "md", lg: "large" }}
-						color={colorParrafo}
-					>
+						fontSize={{
+							base: "sm",
+							md: "md",
+							lg: "large",
+						}}
+						color={colorParrafo}>
 						{parrafo}
 					</Text>
 					<Stack
@@ -60,8 +65,7 @@ export default function HeroBackOffice({
 						spacing={3}
 						align={"center"}
 						alignSelf={"center"}
-						position={"relative"}
-					>
+						position={"relative"}>
 						<OpcionesDelBackOffice />
 					</Stack>
 				</Stack>
