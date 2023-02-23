d6e684bc52b94226ab62245670c039d28dd19d04
Matuzay32 <eze.rey92@gmail.com>
2023-02-23 00:22:55 -0300

    probando delploy

--- pages/backOffice/invitados.js
+++ pages/backOffice/invitados.js
@@ -7,50 +7,53 @@ import swal from "sweetalert";
 import { obtenerTokenYRol } from "../../constantes/tokenLogica";
 
 export default function listaMusica() {
-	const [tokenRol, setTokenRol] = useState();
-	useEffect(() => {
-		obtenerTokenYRol().then((x) => {
-			setTokenRol(x);
-			console.log(x);
-			if (!x) {
-				swal({
-					title: "Error",
-					text: "Tiene que ser administrador para poder acceder",
-					icon: "error",
-				});
-			}
-			if (x?.rol === "USER") {
-				swal({
-					title: "Error",
-					text: "Tiene que ser administrador para poder acceder",
-					icon: "error",
-				});
-			}
-		});
-		return () => {};
-	}, []);
+	// const [tokenRol, setTokenRol] = useState();
+	// useEffect(() => {
+	// 	obtenerTokenYRol().then((x) => {
+	// 		setTokenRol(x);
+	// 		console.log(x);
+	// 		if (!x) {
+	// 			swal({
+	// 				title: "Error",
+	// 				text: "Tiene que ser administrador para poder acceder",
+	// 				icon: "error",
+	// 			});
+	// 		}
+	// 		if (x?.rol === "USER") {
+	// 			swal({
+	// 				title: "Error",
+	// 				text: "Tiene que ser administrador para poder acceder",
+	// 				icon: "error",
+	// 			});
+	// 		}
+	// 	});
+	// 	return () => {};
+	// }, []);
 	return (
 		<>
-			{tokenRol && tokenRol.rol === "ADMIN" && tokenRol.token && (
-				<Box
-					// backgroundImage="linear-gradient(white,  #f7d7ac, #B2936C)"
-					backgroundSize={"cover"}
-					backgroundPosition={"50% 50%"}
-					backgroundImage={"url('../images/fondoInvitaciones.jpg')"}
-				>
-					<Box>
-						<HeroBackOffice
-							colorParrafo={"white"}
-							color={"white"}
-							titulo={"Lista de invitados"}
-							parrafo={
-								"Esta es la lista de invitados confirmados para el casamiento"
-							}
-						></HeroBackOffice>
+			{/* {tokenRol &&
+				tokenRol.rol === "ADMIN" &&
+				tokenRol.token && (
+					<Box
+						// backgroundImage="linear-gradient(white,  #f7d7ac, #B2936C)"
+						backgroundSize={"cover"}
+						backgroundPosition={"50% 50%"}
+						backgroundImage={
+							"url('../images/fondoInvitaciones.jpg')"
+						}>
+						<Box>
+							<HeroBackOffice
+								colorParrafo={"white"}
+								color={"white"}
+								titulo={"Lista de invitados"}
+								parrafo={
+									"Esta es la lista de invitados confirmados para el casamiento"
+								}></HeroBackOffice>
+						</Box>
+						<ContenidoListaInvitados></ContenidoListaInvitados>
 					</Box>
-					<ContenidoListaInvitados></ContenidoListaInvitados>
-				</Box>
-			)}
+				)} */}
+			<Box>box</Box>
 		</>
 	);
 }
--- pages/backOffice/listaMusica.js
+++ pages/backOffice/listaMusica.js
@@ -7,50 +7,54 @@ import swal from "sweetalert";
 import { obtenerTokenYRol } from "../../constantes/tokenLogica";
 
 export default function listaMusica() {
-	const [tokenRol, setTokenRol] = useState();
-	useEffect(() => {
-		obtenerTokenYRol().then((x) => {
-			setTokenRol(x);
-			console.log(x);
-			if (!x) {
-				swal({
-					title: "Error",
-					text: "Tiene que ser administrador para poder acceder",
-					icon: "error",
-				});
-			}
-			if (x?.rol === "USER") {
-				swal({
-					title: "Error",
-					text: "Tiene que ser administrador para poder acceder",
-					icon: "error",
-				});
-			}
-		});
-		return () => {};
-	}, []);
+	// const [tokenRol, setTokenRol] = useState();
+	// useEffect(() => {
+	// 	obtenerTokenYRol().then((x) => {
+	// 		setTokenRol(x);
+	// 		console.log(x);
+	// 		if (!x) {
+	// 			swal({
+	// 				title: "Error",
+	// 				text: "Tiene que ser administrador para poder acceder",
+	// 				icon: "error",
+	// 			});
+	// 		}
+	// 		if (x?.rol === "USER") {
+	// 			swal({
+	// 				title: "Error",
+	// 				text: "Tiene que ser administrador para poder acceder",
+	// 				icon: "error",
+	// 			});
+	// 		}
+	// 	});
+	// 	return () => {};
+	// }, []);
 	return (
 		<>
-			{tokenRol && tokenRol.rol === "ADMIN" && tokenRol.token && (
-				<Box
-					backgroundSize={"cover"}
-					backgroundPosition={"50% 50%"}
-					backgroundImage={"url('../images/fondoSpotify.jpg')"}
-					// backgroundImage="linear-gradient(white,  teal, black)"
-				>
-					<Box>
-						<HeroBackOffice
-							colorParrafo={"white"}
-							color={"white"}
-							titulo={"Lista de canciones"}
-							parrafo={
-								"Estas son las sugerencias musicales que ponen los invitados para el casamiento, ideal para arma la playlist perfecta"
-							}
-						></HeroBackOffice>
+			{/* {tokenRol &&
+				tokenRol.rol === "ADMIN" &&
+				tokenRol.token && (
+					<Box
+						backgroundSize={"cover"}
+						backgroundPosition={"50% 50%"}
+						backgroundImage={
+							"url('../images/fondoSpotify.jpg')"
+						}
+						// backgroundImage="linear-gradient(white,  teal, black)"
+					>
+						<Box>
+							<HeroBackOffice
+								colorParrafo={"white"}
+								color={"white"}
+								titulo={"Lista de canciones"}
+								parrafo={
+									"Estas son las sugerencias musicales que ponen los invitados para el casamiento, ideal para arma la playlist perfecta"
+								}></HeroBackOffice>
+						</Box>
+						<ContenidoListaMusica></ContenidoListaMusica>
 					</Box>
-					<ContenidoListaMusica></ContenidoListaMusica>
-				</Box>
-			)}
+				)} */}
+			<Box>box</Box>
 		</>
 	);
 }
