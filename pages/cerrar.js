00871b5725a6c49e3f932ccaaf7906ef30c1f2f7
Matuzay32 <eze.rey92@gmail.com>
2023-02-23 00:26:03 -0300

    probando delploy

--- pages/cerrar.js
+++ pages/cerrar.js
@@ -3,33 +3,33 @@ import swal from "sweetalert";
 import Router from "next/router";
 
 export default function cerrar() {
-	const { push } = Router;
-	useEffect(() => {
-		const closeSession = () => {
-			swal({
-				title: "Estas seguro?",
-				text: "¿Estas seguro que quieres salir de la cuenta? ",
-				icon: "warning",
-				buttons: true,
-				dangerMode: true,
-			}).then((x) => {
-				if (x) {
-					swal("La cuenta se cerrado correctamente", {
-						icon: "success",
-					});
-					sessionStorage.clear();
-					push("/");
-				} else {
-					swal("Usted sigue en la cuenta", {
-						icon: "success",
-					});
-					push("/");
-				}
-			});
-		};
-		closeSession();
-		return () => {};
-	}, []);
+	// const { push } = Router;
+	// useEffect(() => {
+	// 	const closeSession = () => {
+	// 		swal({
+	// 			title: "Estas seguro?",
+	// 			text: "¿Estas seguro que quieres salir de la cuenta? ",
+	// 			icon: "warning",
+	// 			buttons: true,
+	// 			dangerMode: true,
+	// 		}).then((x) => {
+	// 			if (x) {
+	// 				swal("La cuenta se cerrado correctamente", {
+	// 					icon: "success",
+	// 				});
+	// 				sessionStorage.clear();
+	// 				push("/");
+	// 			} else {
+	// 				swal("Usted sigue en la cuenta", {
+	// 					icon: "success",
+	// 				});
+	// 				push("/");
+	// 			}
+	// 		});
+	// 	};
+	// 	closeSession();
+	// 	return () => {};
+	// }, []);
 	return (
 		<>
 			<div></div>
