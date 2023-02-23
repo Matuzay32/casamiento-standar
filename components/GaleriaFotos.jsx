e2826d569b46efdadb0762e39e82e690fbacafd3
Matuzay32 <eze.rey92@gmail.com>
2023-02-22 22:00:14 -0300

    probando

--- components/GaleriaFotos.jsx
+++ components/GaleriaFotos.jsx
@@ -36,10 +36,6 @@ export default function GaleriaFotos() {
 		},
 		{
 			nombre:
-				"https://images.unsplash.com/photo-1502635385003-ee1e6a1a742d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
-		},
-		{
-			nombre:
 				"https://images.unsplash.com/photo-1484156818044-c040038b0719?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
 		},
 		{
